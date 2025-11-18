91b2c56cc6048eafefdf7ce8a7b14d6357e00a7a
GuiHenry17 <gui201629@gmail.com>
2025-11-18 13:45:54 -0400

    Meu nome endpoint

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Meu nome é Guilherme Henrique Santos Pereira!')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
